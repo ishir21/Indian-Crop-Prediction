d2fee1b3e28f02862b9cbdaf1bdd1fbf2e9c5ee0
ishir21 <ishiragrawal02@gmail.com>
2024-07-08 23:15:10 +0530

    added logging and custom exception error

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)-> List[str]:
+  
+  requirements=[]
+  with open(file_path) as f:
+    requirements = f.readlines()
+    requirements = [req.replace('\n','') for req in requirements]
+    
+    if HYPEN_E_DOT in requirements:
+      requirements.remove(HYPEN_E_DOT)
+      
+  return requirements
+      
+      
+setup(
+  name='crop_prediction',
+  version='0.0.1',
+  author='Ishir',
+  author_email='ishiragrawal02@gmail.com',
+  packages=find_packages(),
+  install_requires = get_requirements('requirements.txt'),
+)
