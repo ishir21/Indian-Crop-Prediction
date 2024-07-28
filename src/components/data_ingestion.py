2292a1d39781252f7ea056eebd46e3990d89879f
ishir21 <ishiragrawal02@gmail.com>
2024-07-28 20:28:51 +0530

    data transformation and model trainer implemented

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,12 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
   train_data_path: str = os.path.join('artifacts','train.csv')
@@ -45,4 +51,10 @@ class DataIngestion:
     
 if __name__ == '__main__':
   obj = DataIngestion()
-  obj.initiate_data_ingestion()
+  train_data, test_data = obj.initiate_data_ingestion()
+  
+  data_transformation = DataTransformation()
+  train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+  
+  modelTrainer = ModelTrainer()
+  print(modelTrainer.initiate_model_trainer(train_arr,test_arr))
